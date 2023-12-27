2dc18fc7e57eb581cdbd195c5b6d4e4e8a72b70f
Darius <dariuskumza@gmail.com>
2023-12-27 19:38:02 +0200

    Update ESLint configuration and add API endpoint for retrieving posts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 const app = express();
 
@@ -13,9 +14,31 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json('Hello World!');
+   res.json('Hello World!');
+});
+
+// GET /api/posts - get all posts
+// SELECT * FROM `posts`
+app.get('/api/posts', async (req, res) => {
+   try {
+      // log in
+      const connection = await mysql.createConnection({
+         database: 'bit_main',
+         host: 'localhost',
+         user: 'root',
+         password: '',
+      });
+      // returns rows
+      const [rows, fields] = await connection.query('SELECT * FROM `posts`');
+      res.json(rows);
+      // log out
+      connection.end();
+   } catch (error) {
+      console.warn('/api/posts', error);
+      res.status(500).json('something wrong');
+   }
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+   console.log(`Server is listening on port ${port}`);
 });
