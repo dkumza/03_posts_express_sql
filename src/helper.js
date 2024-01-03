b3f0173803f9cb34324acc738411a31d9d35ca00
Darius <dariuskumza@gmail.com>
2024-01-03 21:23:05 +0200

    helper

---
+++ src/helper.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2/promise');
+const { dbConfig } = require('./config');
+
+async function getSqlData(sql, argArr = []) {
+   let connection;
+   try {
+      connection = await mysql.createConnection(dbConfig); // connecting to DB
+      const [rows] = await connection.execute(sql, argArr); // execute task
+      return [rows, null];
+   } catch (error) {
+      console.warn('getSqlData', error);
+      return [null, error];
+   } finally {
+      if (connection) connection.end(); // disconnecting
+      console.log('after connection end');
+   }
+}
+
+module.exports = {
+   getSqlData,
+};
